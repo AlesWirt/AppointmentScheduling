c1e16a7ce7f0f97c19f78c6d22b72bfb180bca20
Ales Wirt <sanichwi@gmail.com>
2021-07-20 17:28:29 +0300

    Add partial views and Identity to Startup.cs

--- AppointmentScheduling/Startup.cs
+++ AppointmentScheduling/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,8 @@ namespace AppointmentScheduling
                 options => options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
