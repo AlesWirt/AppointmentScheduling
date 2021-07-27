94cfb7121c146d32bd96e07f3a77fca5698abb25
Ales Wirt <sanichwi@gmail.com>
2021-07-27 21:01:34 +0300

    Add validation to _Layout.html

--- AppointmentScheduling/Models/ViewModels/RegisterViewModel.cs
+++ AppointmentScheduling/Models/ViewModels/RegisterViewModel.cs
@@ -8,20 +8,23 @@ namespace AppointmentScheduling.Models.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Empty field.")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Wrong email.")]
         [EmailAddress]
         public string Email { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage ="This field required password.")]
         [DataType(DataType.Password)]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
+        
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and the confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage ="Please, choose role")]
         [Display(Name = "Role name")]
         public string RoleName { get; set; }
     }
