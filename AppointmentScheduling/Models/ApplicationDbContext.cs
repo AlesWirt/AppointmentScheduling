208aeafb7c934c8a6f8083ac50abe4d0258b7f21
Ales Wirt <sanichwi@gmail.com>
2021-07-29 20:54:49 +0300

    Configure Application User to Identity

--- AppointmentScheduling/Models/ApplicationDbContext.cs
+++ AppointmentScheduling/Models/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace AppointmentScheduling.Models
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
