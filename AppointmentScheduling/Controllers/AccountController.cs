f0dec2d91ccc17cdc7b942f4683e7ae99d2451a6
Ales Wirt <sanichwi@gmail.com>
2021-07-29 21:18:01 +0300

    Register Post Action Method

--- AppointmentScheduling/Controllers/AccountController.cs
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using AppointmentScheduling.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppointmentScheduling.Models.ViewModels;
 
 namespace AppointmentScheduling.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public AccountController(ApplicationDbContext db)
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
         public IActionResult Login()
         {
@@ -22,6 +31,28 @@ namespace AppointmentScheduling.Controllers
 
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return View();
         }
     }
