d897d7e81113de50fdf8f82abb3615c3a8a2aa7e
Ales Wirt <sanichwi@gmail.com>
2021-08-02 17:56:16 +0300

    Add doctors drop down

--- AppointmentScheduling/Controllers/AppointmentController.cs
+++ AppointmentScheduling/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AppointmentScheduling.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,14 @@ namespace AppointmentScheduling.Controllers
 {
     public class AppointmentController : Controller
     {
+        private IAppointmentService _appointmentService;
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
         public IActionResult Index()
         {
+            ViewBag.DoctorList = _appointmentService.GetDoctorList();
             return View();
         }
     }
