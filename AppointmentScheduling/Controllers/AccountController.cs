b7d141ea46359ff87df8fa90b865f5a5f29caae8
Ales Wirt <sanichwi@gmail.com>
2021-08-02 12:08:17 +0300

    Add registration logic with saving to DB

--- AppointmentScheduling/Controllers/AccountController.cs
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AppointmentScheduling.Models.ViewModels;
+using AppointmentScheduling.Utility;
 
 namespace AppointmentScheduling.Controllers
 {
@@ -29,8 +30,14 @@ namespace AppointmentScheduling.Controllers
             return View();
         }
 
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
+            if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Doctor));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Patient));
+            }
             return View();
         }
 
@@ -49,6 +56,7 @@ namespace AppointmentScheduling.Controllers
                 var result = await _userManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("Index", "Home");
                 }
