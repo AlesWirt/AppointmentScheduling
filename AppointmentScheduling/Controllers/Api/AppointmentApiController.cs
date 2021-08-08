eb1e3561d00cea42cfd3e159e727ff70cdbe196b
Ales Wirt <sanichwi@gmail.com>
2021-08-08 12:42:02 +0300

    Add api endpoint and create appointments to DB

---
+++ AppointmentScheduling/Controllers/Api/AppointmentApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using AppointmentScheduling.Services;
+using System;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers.Api
+{
+    [Route("api/Appointment")]
+    [ApiController]
+    public class AppointmentApiController : Controller
+    {
+        private readonly IAppointmentService _appointmentService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _loginUserId;
+        private readonly string _role;
+        public AppointmentApiController(IAppointmentService appointmentService,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _appointmentService = appointmentService;
+            _httpContextAccessor = httpContextAccessor;
+            _loginUserId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            _role = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
